d9132218d7a56752cc31ca3237b6cfa2d8663a7d
Kesing Huang <kesing.huang@gmail.com>
2020-08-14 14:05:59 +0200

    Updates

---
+++ precache-manifest.609d98c72dca9202d1993e48307069a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a503e4ce5c7047f6efbcbb637cc56963",
+    "url": "/Kesing_Huang_React/index.html"
+  },
+  {
+    "revision": "88d8f769c6ed1d639ed7",
+    "url": "/Kesing_Huang_React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a01cbf39dd1340c9abd1",
+    "url": "/Kesing_Huang_React/static/js/2.ff1e6feb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kesing_Huang_React/static/js/2.ff1e6feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d8f769c6ed1d639ed7",
+    "url": "/Kesing_Huang_React/static/js/main.aa9e1358.chunk.js"
+  },
+  {
+    "revision": "49706cfb7b6728ea009b",
+    "url": "/Kesing_Huang_React/static/js/runtime-main.d256e96b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kesing_Huang_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
