949a24f5de688e299653e995a7b36da65b9f737b
Kesing Huang <kesing.huang@gmail.com>
2020-08-27 15:27:08 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
+// import background from './images/Rectangle.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        Hello World
-        </p>
-        
+    <div className="app">
+      
+      <h1 className="title">KesingHuang</h1>
+     <p className="intro">
+     I am a frontend developer who has an eye on design and UX.
+     </p>
+      <div className="polygon">
 
-      </header>
+      </div>  
+      
+      
     </div>
   );
 }
