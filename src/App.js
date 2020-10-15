dcec0104b43bd7e49751a4d04d32fcc24f2e2274
Kesing Huang <kesing.huang@gmail.com>
2020-10-15 14:27:48 +0200

    under construction msg

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="title">KesingHuang</h1>
+      <div className="temperate">UNDER CONSTRUCTION</div>
       <div className="polygon"></div>
       <p className="intro">
         I am a frontend developer who has an eye on design and UX.
